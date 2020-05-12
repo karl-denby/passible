f24d45a6b9bfc29ff53bc3878ac6b4370dfd0d4a
Karl Denby <karl.denby@gmail.com>
2020-05-12 23:02:15 +0100

    test buttons with click handlers

--- src/render.js
+++ src/render.js
@@ -1,11 +1,25 @@
 const { remote } = require('electron');
 const { dialog, Menu } = remote;
 
-const clickMeBtn = document.getElementById('clickMe');
-
-// ClickMeBtn increament fuction
+const btnCreateVM = document.getElementById('btnCreateVM');
+const btnConfigureVM = document.getElementById('btnConfigureVM');
+const btnDeleteVM = document.getElementById('btnDeleteVM');
 let total = 0;
-clickMeBtn.onclick = e => {
+
+// binCreateVM increament fuction
+btnCreateVM.onclick = e => {
     total++
-    clickMeBtn.innerText = `You clicked me ${total} times`
+    btnCreateVM.innerText = `You clicked me ${total} times`
+}
+
+// binConfigureVM increament fuction
+btnConfigureVM.onclick = e => {
+    total++
+    btnConfigureVM.innerText = `You clicked me ${total} times`
+}
+
+// binDeleteVM increament fuction
+btnDeleteVM.onclick = e => {
+    total++
+    btnDeleteVM.innerText = `You clicked me ${total} times`
 }
