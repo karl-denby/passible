1c55b308c5819d5c5280e03a6d83dca5fe79851f
Karl Denby <karl.denby@gmail.com>
2020-05-14 23:58:12 +0100

    create POC

--- src/render.js
+++ src/render.js
@@ -1,25 +1,60 @@
-const { remote } = require('electron');
+const { remote } = require("electron");
 const { dialog, Menu } = remote;
+const { exec } = require("child_process");
 
-const btnCreateVM = document.getElementById('btnCreateVM');
-const btnConfigureVM = document.getElementById('btnConfigureVM');
-const btnDeleteVM = document.getElementById('btnDeleteVM');
+const btnCreateVM = document.getElementById("btnCreateVM");
+const btnConfigureVM = document.getElementById("btnConfigureVM");
+const btnDeleteVM = document.getElementById("btnDeleteVM");
+const txtOutput = document.getElementById("txtOutput");
 let total = 0;
 
-// binCreateVM increament fuction
-btnCreateVM.onclick = e => {
-    total++
-    btnCreateVM.innerText = `You clicked me ${total} times`
-}
+btnCreateVM.onclick = (e) => {
+  const createCmd =
+    "multipass launch --disk 6G --mem 512m --cpus 1 --name passible0";
+
+  txtOutput.innerHTML = `Please wait, while we run the command: ${createCmd}`;
+  exec(createCmd, (error, stdout, stderr) => {
+    if (error) {
+      txtOutput.innerHTML = `error: ${error.message}`;
+      return;
+    }
+    if (stderr) {
+      txtOutput.innerHTML = `stderr: ${stderr}`;
+      return;
+    }
+    txtOutput.innerHTML = `done: ${stdout}`;
+  });
+};
 
 // binConfigureVM increament fuction
-btnConfigureVM.onclick = e => {
-    total++
-    btnConfigureVM.innerText = `You clicked me ${total} times`
-}
+btnConfigureVM.onclick = (e) => {
+  configCmd = `ansible all -i 'ubuntu@192.168.122.109,' -a "ufw status" -b`
+  txtOutput.innerHTML = `Please wait, while we run the command: ${configCmd}`;
+  exec(configCmd, (error, stdout, stderr) => {
+    if (error) {
+      txtOutput.innerHTML = `error: ${error.message}`;
+      return;
+    }
+    if (stderr) {
+      txtOutput.innerHTML = `stderr: ${stderr}`;
+      return;
+    }
+    txtOutput.innerHTML = `done: ${stdout}`;
+  });
+};
 
 // binDeleteVM increament fuction
-btnDeleteVM.onclick = e => {
-    total++
-    btnDeleteVM.innerText = `You clicked me ${total} times`
-}
+btnDeleteVM.onclick = (e) => {
+  const deleteCmd = "multipass delete passible0 && multipass purge";
+  exec(deleteCmd, (error, stdout, stderr) => {
+    if (error) {
+      txtOutput.innerHTML = `error: ${error.message}`;
+      return;
+    }
+    if (stderr) {
+      txtOutput.innerHTML = `stderr: ${stderr}`;
+      return;
+    }
+    txtOutput.innerHTML = `done: ${stdout}`;
+  });
+};
