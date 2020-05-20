87c279ba5ae71873e51e9e7dc8c5b3567ce3bb3f
Karl Denby <karl.denby@gmail.com>
2020-05-20 22:53:42 +0100

    discovery is more reliable

--- src/render.js
+++ src/render.js
@@ -15,12 +15,13 @@ let targetVM = {}
 
 function getAllVmInfo() {
   const list = "multipass list"
-  exec(list, (error, stdout, stderr) => {
+  let data = []
+  
+  const run = exec(list, (error, stdout, stderr) => {
     if (error) return error;
     if (stderr) return stderr;
 
     const txt_lines = stdout.split("\n")
-    let data = []
     
     txt_lines.forEach(line => {  
       line = line.replace(/\s+/g, ' ');  // trim multiple spaces
@@ -34,16 +35,24 @@ function getAllVmInfo() {
         })
       }
     });
-    inventory = data
-  });
-}  
+  })
+
+  run.on('exit', (code) => {
+    if (code === 0) {
+      inventory = data
+    } else {
+      inventory = []
+    }
+  })
+};
+
 
 btnDiscoverVM.onclick = (e) => {
-   getAllVmInfo()
-   txtListVM.innerHTML = "Discovered VMs: "
-   inventory.forEach((item, index) => {   
-     txtListVM.innerHTML += ` ${item.name},`
-   })
+  txtListVM.innerHTML = "Discovered VMs: "
+  getAllVmInfo()
+  inventory.forEach((item, index) => {   
+    txtListVM.innerHTML += ` ${item.name}(${item.ipv4}),`
+  })
 };
 
 btnCreateVM.onclick = (e) => {
@@ -52,39 +61,50 @@ btnCreateVM.onclick = (e) => {
     `multipass launch --disk 8G --mem 512m --cpus 1 --name ${targetVM.name}`;
 
   txtOutput.innerHTML = `Please wait, while we run the command: ${createCmd}`;
-  exec(createCmd, (error, stdout, stderr) => {
+  const ssh = exec(createCmd, (error, stdout, stderr) => {
       if (error) { txtOutput.innerHTML = error }
       if (stderr) { txtOutput.innerHTML = stderr }
       if (stdout) { txtOutput.innerHTML = stdout }
   });
-  getAllVmInfo()
+
+  ssh.on('exit', (code) => {
+    getAllVmInfo()
+  })
+
 };
 
 btnConfigureVM.onclick = (e) => {
   targetVM.name = txtCreateVM.value
-  exec(`multipass copy-files ~/.ssh/id_rsa.pub ${targetVM.name}:/home/ubuntu/.ssh/authorized_keys`, (error, stdout, stderr) => {
+  targetVM.ipv4 = inventory.find(vm => vm.name === targetVM.name).ipv4
+  
+  const ssh = exec(`multipass copy-files ~/.ssh/id_rsa.pub ${targetVM.name}:/home/ubuntu/.ssh/authorized_keys`, (error, stdout, stderr) => {
     if (error) { console.log(error) }
     if (stderr) { console.log(stderr) }
     if (stdout) { console.log(stdout) }
   });
 
-  const configCmd = `env ANSIBLE_HOST_KEY_CHECKING=false ansible all -i 'ubuntu@${targetVM.ipv4},' -m setup -b -e '{"ansible_python_interpreter":"/usr/bin/python3"}'`
-  txtOutput.innerHTML = `Please wait, while we run the command: ${configCmd}`;
-
-  exec(configCmd, (error, stdout, stderr) => {
-    if (error) { txtOutput.innerHTML = `error: ${error.message}` }
-    if (stderr) { txtOutput.innerHTML = `stderr: ${stderr}` }
-    txtOutput.innerHTML = `done: ${stdout}`;
-  });
+  ssh.on('exit', (code) => {
+    const configCmd = `env ANSIBLE_HOST_KEY_CHECKING=false ansible all -i 'ubuntu@${targetVM.ipv4},' -m setup -b -e '{"ansible_python_interpreter":"/usr/bin/python3"}'`
+    txtOutput.innerHTML = `Please wait, while we run the command: ${configCmd}`;
+  
+    exec(configCmd, (error, stdout, stderr) => {
+      if (error) { txtOutput.innerHTML = `error: ${error.message}` }
+      if (stderr) { txtOutput.innerHTML = `stderr: ${stderr}` }
+      txtOutput.innerHTML = `done: ${stdout}`;
+    });  
+  })
 
 };
 
 btnDeleteVM.onclick = (e) => {
   const deleteCmd = `multipass delete ${txtCreateVM.value} && multipass purge`;
-  exec(deleteCmd, (error, stdout, stderr) => {
+  const ssh = exec(deleteCmd, (error, stdout, stderr) => {
     if (error) { txtOutput.innerHTML = `error: ${error.message}` }
     if (stderr) { txtOutput.innerHTML = `stderr: ${stderr}` }
     txtOutput.innerHTML = `done: ${stdout}`;
   });
-  getAllVmInfo()
-};
+
+  ssh.on('exit', (code) => {
+    getAllVmInfo()
+  })
+}
