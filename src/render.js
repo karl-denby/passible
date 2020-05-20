0def9ca5452c326c155a349f5437f13193743beb
Karl Denby <karl.denby@gmail.com>
2020-05-20 16:35:54 +0100

    discover on, create/delete

--- src/render.js
+++ src/render.js
@@ -2,27 +2,49 @@ const { remote } = require("electron");
 const { dialog, Menu } = remote;
 const { exec } = require("child_process");
 
+const btnDiscoverVM = document.getElementById("btnDiscoverVM");
+const txtListVM = document.getElementById("txtListVM");
+
 const btnCreateVM = document.getElementById("btnCreateVM");
 const txtCreateVM = document.getElementById("txtCreateVM");
 const btnConfigureVM = document.getElementById("btnConfigureVM");
 const btnDeleteVM = document.getElementById("btnDeleteVM");
 const txtOutput = document.getElementById("txtOutput");
+let inventory = []
 let targetVM = {}
 
-function getVmInfo(mp_list, vm_name) {
-  console.log('running')
-  let txt_lines = mp_list.split("\n")
-  let data = {}
+function getAllVmInfo() {
+  const list = "multipass list"
+  exec(list, (error, stdout, stderr) => {
+    if (error) return error;
+    if (stderr) return stderr;
 
-  txt_lines.forEach(line => {
-    line = line.replace(/\s+/g, ' ');  // trim multiple spaces to a single space
-    line = line.split(" ")
-    if (line[0] === vm_name) {
-      data =  { name: line[0], state: line[1], ipv4: line[2], image: line[3] }
-    }
+    const txt_lines = stdout.split("\n")
+    let data = []
+    
+    txt_lines.forEach(line => {  
+      line = line.replace(/\s+/g, ' ');  // trim multiple spaces
+      line = line.split(" ")
+      if ((line[0] != '' && line[0] != 'Name' && line[0] != "No")) {
+        data.push({ 
+          "name": line[0], 
+          "state": line[1], 
+          "ipv4": line[2], 
+          "image": line[3]
+        })
+      }
+    });
+    inventory = data
   });
-  return data
-}
+}  
+
+btnDiscoverVM.onclick = (e) => {
+   getAllVmInfo()
+   txtListVM.innerHTML = "Discovered VMs: "
+   inventory.forEach((item, index) => {   
+     txtListVM.innerHTML += ` ${item.name},`
+   })
+};
 
 btnCreateVM.onclick = (e) => {
   targetVM.name = txtCreateVM.value
@@ -35,18 +57,11 @@ btnCreateVM.onclick = (e) => {
       if (stderr) { txtOutput.innerHTML = stderr }
       if (stdout) { txtOutput.innerHTML = stdout }
   });
+  getAllVmInfo()
 };
 
 btnConfigureVM.onclick = (e) => {
-  exec(`multipass list`, (error, stdout, stderr) => {
-    if (error) { txtOutput.innerHTML = error }
-    if (stderr) { txtOutput.innerHTML = stderr }
-    if (stdout) {
-      txtOutput.innerHTML = stdout
-      targetVM = getVmInfo(stdout, txtCreateVM.value)
-    }
-  });
-
+  targetVM.name = txtCreateVM.value
   exec(`multipass copy-files ~/.ssh/id_rsa.pub ${targetVM.name}:/home/ubuntu/.ssh/authorized_keys`, (error, stdout, stderr) => {
     if (error) { console.log(error) }
     if (stderr) { console.log(stderr) }
@@ -71,4 +86,5 @@ btnDeleteVM.onclick = (e) => {
     if (stderr) { txtOutput.innerHTML = `stderr: ${stderr}` }
     txtOutput.innerHTML = `done: ${stdout}`;
   });
+  getAllVmInfo()
 };
