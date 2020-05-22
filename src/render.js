3f0752cd7465c59d53a19e8599ac1ed0a839efa9
Karl Denby <karl.denby@gmail.com>
2020-05-22 23:09:54 +0100

    return correct ansible inventory

--- src/render.js
+++ src/render.js
@@ -13,8 +13,15 @@ const txtOutput = document.getElementById("txtOutput");
 var inventory = [];
 var targetVM = {};
 
-function ansibleInventory() {
-  return 'ubuntu@10.203.58.74,ubuntu@10.203.58.249,ubuntu@10.203.58.239,'
+function ansibleInventoryAll() {
+  const multipass_list = getAllVmInfo()
+
+  //return 'ubuntu@10.203.58.74,ubuntu@10.203.58.249,ubuntu@10.203.58.239,'
+  let ansible_inventory = [];
+  multipass_list.forEach(vm => {
+    ansible_inventory.push(`ubuntu@${vm.ipv4},`)
+  });
+  return ansible_inventory;
 }
 
 function getAllVmInfo() {
@@ -90,34 +97,38 @@ btnConfigureVM.onclick = (e) => {
   });
 
   mp_copy.on('exit', (code) => {
-    const ssh = exec(`multipass exec ${targetVM.name} -- cat /home/ubuntu/.ssh/new_key >> /home/ubuntu/.ssh/authorized_keys`, (error, stdout, stderr) => {
-      if (error) { console.log(error) }
-      if (stderr) { console.log(stderr) }
-      if (stdout) { console.log(stdout) }
+    console.log(`Copy command finished with code: ${code}`)
+  })
+    
+  const sed = exec(`multipass exec ${targetVM.name} -- sed -i '$r /home/ubuntu/.ssh/new_key' /home/ubuntu/.ssh/authorized_keys`, (error, stdout, stderr) => {
+    if (error) { console.log(error) }
+    if (stderr) { console.log(stderr) }
+    
+    sed.on('exit', (code) => {
+      console.log(`sed command finished with code: ${code}`)
+    })
+
+    // We want to run this playbook via ansible command
+    const ENV = 'env ANSIBLE_HOST_KEY_CHECKING=false';
+    const playbook = __dirname + '/playbooks/hostnames.ansible';
+    const inventory = ansibleInventoryAll();
+    const extras = '{"ansible_python_interpreter":"/usr/bin/python3"}';
+    const cmd = `${ENV} ansible-playbook -i ${inventory} -e ${extras} ${playbook}`;
+
+    const configCmd = `env ANSIBLE_HOST_KEY_CHECKING=false ansible all -i 'ubuntu@${targetVM.ipv4},' -m setup -b -e '{"ansible_python_interpreter":"/usr/bin/python3"}'`
+    txtOutput.innerHTML = `Please wait, while we run the command: ${cmd}`;
+
+    const ansible = exec(cmd, (error, stdout, stderr) => {
+      if (error) { txtOutput.innerHTML = `error: ${error}` }
+      if (stderr) { txtOutput.innerHTML = `stderr: ${stderr}` }
+      if (stdout) { txtOutput.innerHTML = `done: ${stdout}`; }
     });
 
-    ssh.on('exit', (code) => {
-      // We want to run this playbook via ansible command
-      const ENV = 'env ANSIBLE_HOST_KEY_CHECKING=false';
-      const playbook = __dirname + '/playbooks/hostnames.ansible';
-      const inventory = ansibleInventory();
-      const extras = '{"ansible_python_interpreter":"/usr/bin/python3"}';
-      const cmd = `${ENV} ansible-playbook -i ${inventory} -e ${extras} ${playbook}`;
-
-      const configCmd = `env ANSIBLE_HOST_KEY_CHECKING=false ansible all -i 'ubuntu@${targetVM.ipv4},' -m setup -b -e '{"ansible_python_interpreter":"/usr/bin/python3"}'`
-      txtOutput.innerHTML = `Please wait, while we run the command: ${configCmd}`;
-
-      const ansible = exec(cmd, (error, stdout, stderr) => {
-        if (error) { txtOutput.innerHTML = `error: ${error.message}` }
-        if (stderr) { txtOutput.innerHTML = `stderr: ${stderr}` }
-        if (stdout) { txtOutput.innerHTML = `done: ${stdout}`; }
-      });
-
-      ansible.on('exit', (code) => {
-        console.log(`ansible exit code: ${code}`);
-      })
+    ansible.on('exit', (code) => {
+      console.log(`ansible exit code: ${code}`);
     })
   })
+
 };
 
 btnDeleteVM.onclick = (e) => {
