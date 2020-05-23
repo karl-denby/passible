bb1562ce970c0e9b8ec994b6ab54a95393a5a223
Karl Denby <karl.denby@gmail.com>
2020-05-23 16:06:43 +0100

    sequence config commands on previous exit

--- src/render.js
+++ src/render.js
@@ -10,13 +10,17 @@ const txtCreateVM = document.getElementById("txtCreateVM");
 const btnConfigureVM = document.getElementById("btnConfigureVM");
 const btnDeleteVM = document.getElementById("btnDeleteVM");
 const txtOutput = document.getElementById("txtOutput");
-var inventory = [];
+
+var mp_list = '';
+var mp_list_array = [];
+var mp_list_string = '';
+var ansible_inventory = '';
+
 var targetVM = {};
 
 function ansibleInventoryAll() {
   const multipass_list = getAllVmInfo()
 
-  //return 'ubuntu@10.203.58.74,ubuntu@10.203.58.249,ubuntu@10.203.58.239,'
   let ansible_inventory = [];
   multipass_list.forEach(vm => {
     ansible_inventory.push(`ubuntu@${vm.ipv4},`)
@@ -24,48 +28,64 @@ function ansibleInventoryAll() {
   return ansible_inventory;
 }
 
-function getAllVmInfo() {
-  const list = "multipass list";
+function multipassList() {
+  const cmd = "multipass list";
+  let inventory = '';
+  
+  const multipass_list = exec(cmd, (error, stdout, stderr) => {
+    if (error) mutateDiscover(error);
+    if (stderr) mutateDiscover(stderr);
+    if (stdout) mp_list = stdout;
+  })
+};
+
+function mutateStatus(message) {
+  txtOutput.innerHTML = '';
+  txtOutput.innerHTML = message;
+}
+
+function mutateDiscover(message) {
+  txtListVM.innerHTML = '';
+  txtListVM.innerHTML = message;
+}
+
+function convertListToObject(out) {
+  const txt_lines = out.split("\n")
   let data = [];
 
-  const run = exec(list, (error, stdout, stderr) => {
-    if (error) return error;
-    if (stderr) return stderr;
-
-    const txt_lines = stdout.split("\n")
-
-    txt_lines.forEach(line => {
-      line = line.replace(/\s+/g, ' ');  // trim multiple spaces
-      line = line.split(" ");
-      if ((line[0] != '' && line[0] != 'Name' && line[0] != "No")) {
-        data.push({
-          "name": line[0],
-          "state": line[1],
-          "ipv4": line[2],
-          "image": line[3]
-        })
-      }
-    });
-  })
-
-  run.on('exit', (code) => {
-    if (code === 0) {
-      inventory = data
-    } else {
-      inventory = []
+  txt_lines.forEach(line => {
+    line = line.replace(/\s+/g, ' ');  // trim multiple spaces
+    line = line.split(" ");
+    if ((line[0] != '' && line[0] != 'Name' && line[0] != "No")) {
+      data.push({
+        "name": line[0],
+        "state": line[1],
+        "ipv4": line[2],
+        "image": line[3]
+      })
     }
-  })
-  return inventory;
-};
-
+  });
+  mp_list_array = data;
+}
 
 btnDiscoverVM.onclick = (e) => {
-  txtListVM.innerHTML = "Discovered VMs: ";
-  getAllVmInfo();
-  inventory.forEach((item, index) => {
-    txtListVM.innerHTML += ` ${item.name}(${item.ipv4}),`
+  mp_list_string = ``;  
+  ansible_inventory = ``;
+  multipassList();
+  convertListToObject(mp_list);
+  mp_list_array.forEach((item, index) => {
+    mp_list_string += ` ${item.name}(${item.ipv4}),`
   })
+
+  ansible_inventory = [];
+  mp_list_array.forEach(vm => {
+    ansible_inventory.push(` ubuntu@${vm.ipv4}`)
+  });
+  ansible_inventory += ','
+  mutateDiscover(mp_list_string);
+  mutateStatus(ansible_inventory);
 };
+
 
 btnCreateVM.onclick = (e) => {
   targetVM.name = txtCreateVM.value;
@@ -78,58 +98,63 @@ btnCreateVM.onclick = (e) => {
       if (stderr) { txtOutput.innerHTML = stderr }
       if (stdout) { txtOutput.innerHTML = stdout }
   });
-
-  ssh.on('exit', (code) => {
-    getAllVmInfo();
-  })
-
 };
 
 btnConfigureVM.onclick = (e) => {
-  targetVM.name = txtCreateVM.value;
-  let local = getAllVmInfo()
-  targetVM.ipv4 = local.find(vm => vm.name === targetVM.name).ipv4;
+  mpCopySshKey()
+}
 
-  const mp_copy = exec(`multipass copy-files ~/.ssh/id_rsa.pub ${targetVM.name}:/home/ubuntu/.ssh/new_key`, (error, stdout, stderr) => {
-    if (error) { console.log(error) }
-    if (stderr) { console.log(stderr) }
-    if (stdout) { console.log(stdout) }
-  });
+function mpCopySshKey(){
+  targetVM.name = txtCreateVM.value;
+  targetVM.ipv4 = mp_list_array.find(vm => vm.name === targetVM.name).ipv4;
+
+  const cmd = `multipass copy-files ~/.ssh/id_rsa.pub ${targetVM.name}:/home/ubuntu/.ssh/new_key`;
+  console.log(cmd);
+  const mp_copy = exec(cmd, (error, stdout, stderr) => {
+    if (error) { txtOutput.innerHTML = `error: ${error}` }
+    if (stderr) { txtOutput.innerHTML = `stderr: ${stderr}` }
+    if (stdout) { txtOutput.innerHTML = `stdout: ${stdout}` }
+  })
 
   mp_copy.on('exit', (code) => {
     console.log(`Copy command finished with code: ${code}`)
+    mpModifyAuthorizeKeys()
+  })
+}
+
+function mpModifyAuthorizeKeys(){
+  targetVM.name = txtCreateVM.value;
+  targetVM.ipv4 = mp_list_array.find(vm => vm.name === targetVM.name).ipv4;
+  const sed = exec(`multipass exec ${targetVM.name} -- sed -i '$r /home/ubuntu/.ssh/new_key' /home/ubuntu/.ssh/authorized_keys`, (error, stdout, stderr) => {
+    if (error) { txtOutput.innerHTML = `error: ${error}` }
+    if (stderr) { txtOutput.innerHTML = `stderr: ${stderr}` }
+    if (stdout) { txtOutput.innerHTML = `stdout: ${stdout}` }
   })
     
-  const sed = exec(`multipass exec ${targetVM.name} -- sed -i '$r /home/ubuntu/.ssh/new_key' /home/ubuntu/.ssh/authorized_keys`, (error, stdout, stderr) => {
-    if (error) { console.log(error) }
-    if (stderr) { console.log(stderr) }
-    
-    sed.on('exit', (code) => {
+  sed.on('exit', (code) => {
       console.log(`sed command finished with code: ${code}`)
-    })
+      apHostnames()
+  })
+}
 
-    // We want to run this playbook via ansible command
+function apHostnames() {
+// We want to run this playbook via ansible command
     const ENV = 'env ANSIBLE_HOST_KEY_CHECKING=false';
     const playbook = __dirname + '/playbooks/hostnames.ansible';
-    const inventory = ansibleInventoryAll();
     const extras = '{"ansible_python_interpreter":"/usr/bin/python3"}';
-    const cmd = `${ENV} ansible-playbook -i ${inventory} -e ${extras} ${playbook}`;
-
-    const configCmd = `env ANSIBLE_HOST_KEY_CHECKING=false ansible all -i 'ubuntu@${targetVM.ipv4},' -m setup -b -e '{"ansible_python_interpreter":"/usr/bin/python3"}'`
+    const cmd = `${ENV} ansible-playbook -i '${ansible_inventory}' -e ${extras} ${playbook}`;
     txtOutput.innerHTML = `Please wait, while we run the command: ${cmd}`;
-
+    
     const ansible = exec(cmd, (error, stdout, stderr) => {
       if (error) { txtOutput.innerHTML = `error: ${error}` }
       if (stderr) { txtOutput.innerHTML = `stderr: ${stderr}` }
-      if (stdout) { txtOutput.innerHTML = `done: ${stdout}`; }
+      if (stdout) { txtOutput.innerHTML = `stdout: ${stdout}`; }
     });
-
+    
     ansible.on('exit', (code) => {
       console.log(`ansible exit code: ${code}`);
     })
-  })
-
-};
+}
 
 btnDeleteVM.onclick = (e) => {
   const deleteCmd = `multipass delete -p ${txtCreateVM.value}`;
@@ -140,6 +165,6 @@ btnDeleteVM.onclick = (e) => {
   });
 
   ssh.on('exit', (code) => {
-    getAllVmInfo();
+    btnDiscoverVM.onclick();
   })
 }
