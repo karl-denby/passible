85189275fc8097b4f3314a6aefb2d8a736cc0983
karl-denby <karl.denby@gmail.com>
2020-05-27 16:56:12 +0100

    tidy up, use dedicated function for mutating gui

--- src/render.js
+++ src/render.js
@@ -1,11 +1,10 @@
 const { remote } = require("electron");
-const { dialog, Menu } = remote;
+//const { dialog, Menu } = remote;
 const { exec } = require("child_process");
 
 const btnDiscoverVM = document.getElementById("btnDiscoverVM");
 const btnSetupVM = document.getElementById("btnSetupVM");
 const txtListVM = document.getElementById("txtListVM");
-
 const btnCreateVM = document.getElementById("btnCreateVM");
 const txtCreateVM = document.getElementById("txtCreateVM");
 const btnConfigureVM = document.getElementById("btnConfigureVM");
@@ -16,7 +15,6 @@ var mp_list = '';
 var mp_list_array = [];
 var mp_list_string = '';
 var ansible_inventory = '';
-
 var targetVM = {};
 
 function mutateStatus(message) {
@@ -24,10 +22,12 @@ function mutateStatus(message) {
   txtOutput.innerHTML = message;
 }
 
+
 function mutateDiscover(message) {
   txtListVM.innerHTML = '';
   txtListVM.innerHTML = message;
 }
+
 
 function convertListToObject(out) {
   const txt_lines = out.split("\n")
@@ -47,6 +47,7 @@ function convertListToObject(out) {
   });
   mp_list_array = data;
 }
+
 
 btnDiscoverVM.onclick = (e) => {
   mp_list_string = ``;
@@ -77,17 +78,18 @@ btnCreateVM.onclick = (e) => {
   const createCmd =
     `multipass launch --disk 4G --mem 512m --cpus 1 --name ${targetVM.name}`;
 
-  txtOutput.innerHTML = `Please wait, while we run the command: ${createCmd}`;
+  mutateStatus(`Please wait, while we run the command: ${createCmd}`);
   const ssh = exec(createCmd, (error, stdout, stderr) => {
-      if (error) { txtOutput.innerHTML = error }
-      if (stderr) { txtOutput.innerHTML = stderr }
-      if (stdout) { txtOutput.innerHTML = stdout }
+      if (error) mutateStatus(error)
+      if (stderr) mutateStatus(stderr)
+      if (stdout) mutateStatus(stdout)
   });
 
   ssh.on('exit', (code) => {
     console.log(`Completed: ${createCmd}`)
   })
 };
+
 
 function runCommands(commands) {
   if (!commands) { return 'no command sequence provided' }
@@ -102,17 +104,13 @@ function runCommands(commands) {
 
   cmd.on('exit', (code) => {
     if (code === 0) {
-      // Delete command zero in the list
       const newCommands = commands.slice(1);
-
-      // Stop working if we have none left
       if (newCommands.length === 0) return 'done'
-
-      // Call ourselfs to do the next iteration with whats left of the list
       runCommands(newCommands);
     }
   });
 } // runCommands(commands)
+
 
 btnSetupVM.onclick = (e) => {
   // Commands in sequential order that they need to be run
@@ -128,63 +126,26 @@ btnSetupVM.onclick = (e) => {
   console.log(result);
 }
 
-function mpCopySshKey(){
-  mp_list_array.forEach(vm => {
-    const cmd = `multipass copy-files ~/.ssh/id_rsa.pub ${vm.name}:/home/ubuntu/.ssh/new_key`;
-    console.log(cmd);
-    const mp_copy = exec(cmd, (error, stdout, stderr) => {
-      if (error) { txtOutput.innerHTML = `error: ${error}` }
-      if (stderr) { txtOutput.innerHTML = `stderr: ${stderr}` }
-      if (stdout) { txtOutput.innerHTML = `stdout: ${stdout}` }
-    })
-
-    mp_copy.on('exit', (code) => {
-      console.log(`Copy command finished with code: ${code}`)
-    })
-
-  });
-
-  mpModifyAuthorizeKeys();
-}
-
-function mpModifyAuthorizeKeys(){
-  mp_list_array.forEach(vm => {
-
-    const sed = exec(`multipass exec ${vm.name} -- sed -i '$r /home/ubuntu/.ssh/new_key' /home/ubuntu/.ssh/authorized_keys`, (error, stdout, stderr) => {
-      if (error) { txtOutput.innerHTML = `error: ${error}` }
-      if (stderr) { txtOutput.innerHTML = `stderr: ${stderr}` }
-      if (stdout) { txtOutput.innerHTML = `stdout: ${stdout}` }
-    })
-
-    sed.on('exit', (code) => {
-      console.log(`sed command finished with code: ${code}`)
-    })
-
-  });
-
-  apHostnames();
-}
 
 function apHostnames() {
-
   const ENV = 'env ANSIBLE_HOST_KEY_CHECKING=false';
   const playbook = __dirname + '/playbooks/hostnames.ansible';
   const extras = '{"ansible_python_interpreter":"/usr/bin/python3"}';
   const cmd = `${ENV} ansible-playbook -i '${ansible_inventory}' -e ${extras} ${playbook}`;
 
-  txtOutput.innerHTML = `Please wait, while we run the command: ${cmd}`;
+  mutateStatus(`Please wait, while we run the command: ${cmd}`);
 
   const ansible = exec(cmd, (error, stdout, stderr) => {
-    if (error) { txtOutput.innerHTML = `error: ${error}` }
-    if (stderr) { txtOutput.innerHTML = `stderr: ${stderr}` }
-    if (stdout) { txtOutput.innerHTML = `stdout: ${stdout}`; }
+    if (error) mutateStatus(`error: ${error}`)
+    if (stderr) mutateStatus(`stderr: ${stderr}`)
+    if (stdout) mutateStatus(`stdout: ${stdout}`)
   });
 
   ansible.on('exit', (code) => {
     console.log(`ansible exit code: ${code}`);
   })
-
 }
+
 
 btnConfigureVM.onclick = (e) => {
   targetVM.name = txtCreateVM.value
@@ -199,33 +160,30 @@ btnConfigureVM.onclick = (e) => {
   const extras = '{"ansible_python_interpreter":"/usr/bin/python3"}';
   const cmd = `${ENV} ansible-playbook -i 'ubuntu@${targetVM.ipv4},' -e ${extras} ${playbook}`;
 
-  txtOutput.innerHTML = `Please wait, while we run the command: ${cmd}`;
+  mutateStatus(`Please wait, while we run the command: ${cmd}`);
 
   const ansible = exec(cmd, (error, stdout, stderr) => {
-    if (error) { txtOutput.innerHTML = `error: ${error}` }
-    if (stderr) { txtOutput.innerHTML = `stderr: ${stderr}` }
-    if (stdout) { txtOutput.innerHTML = `stdout: ${stdout}`; }
+    if (error) mutateStatus(`error: ${error}`)
+    if (stderr) mutateStatus(`stderr: ${stderr}`)
+    if (stdout) mutateStatus(`stdout: ${stdout}`)
   });
 
   ansible.on('exit', (code) => {
     console.log(`ansible exit code: ${code}`);
   })
-
 }
 
-btnDeleteVM.onclick = (e) => {
 
+btnDeleteVM.onclick = (e) => {
   const deleteCmd = `multipass delete -p ${txtCreateVM.value}`;
 
   const ssh = exec(deleteCmd, (error, stdout, stderr) => {
-    if (error) { txtOutput.innerHTML = `error: ${error.message}` }
-    if (stderr) { txtOutput.innerHTML = `stderr: ${stderr}` }
-    if (stdout) { txtOutput.innerHTML = `done: ${stdout}`; }
+    if (error) mutateStatus(`error: ${error}`)
+    if (stderr) mutateStatus(`stderr: ${stderr}`)
+    if (stdout) mutateStatus(`stdout: ${stdout}`)
   });
 
   ssh.on('exit', (code) => {
-    setTimeout(function(){ console.log("Pausing for Delete"); }, 5000);
-    btnDiscoverVM.onclick();
+    setTimeout(function(){ btnDiscoverVM.onclick(); }, 5000);
   })
-
 }
