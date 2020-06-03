7a06f1ec0118828b0d713d1b0405f27587fd178d
Karl Denby <karl.denby@gmail.com>
2020-06-03 11:25:13 +0100

    return output from last command executed

--- src/render.js
+++ src/render.js
@@ -12,19 +12,37 @@ const btnDeleteVM = document.getElementById("btnDeleteVM")
 const txtOutput = document.getElementById("txtOutput")
 
 
+function runCommands(commands, callback) {
+  if (!commands) { console.error('no command sequence provided')}
+  
+  // Run command[0] if its the last/only in the list invoke callback to deal with results
+  const cmd = exec(commands[0], (error, stdout, stderr) => {
+    if (error && commands.length === 1) { callback(error) }
+    if (stderr && commands.length === 1) { callback(stderr) }
+    if (stdout && commands.length === 1) { callback(stdout) }
+  });
+
+  // If we have more commands to run slice 0 of the front and start execution again
+  cmd.on('close', (code) => {
+    if (code === 0) {
+      const newCommands = commands.slice(1)
+      if (newCommands.length != 0) { runCommands(newCommands) } 
+  }});
+
+} 
+
+
 function mutateStatus(message) {
-  txtOutput.innerHTML = ''
-  txtOutput.innerHTML = message
+  txtOutput.innerHTML = message + ','
 }
 
 
 function mutateDiscover(message) {
-  txtListVM.innerHTML = ''
   txtListVM.innerHTML = message
 }
 
 
-function convertListToObject(out) {
+function mpListAsObject(out) {
   const txt_lines = out.split("\n")
   let data = []
 
@@ -49,25 +67,25 @@ function convertListToObject(out) {
 
 btnDiscoverVM.onclick = (e) => {
 
-  const mp_list = runCommands([`multipass list`])
-  const mp_inventory = convertListToObject(mp_list)
-
-  let mp_list_string = ''
-  mp_inventory.forEach((item, index) => {
-    mp_list_string += ` ${item.name}(${item.ipv4}),`
+  let mp_list = []
+  runCommands([`multipass list`], function(mp_list) {
+    
+    const mp_inventory = mpListAsObject(mp_list)
+    
+    let mp_list_string = ''
+    mp_inventory.forEach((item, index) => {
+      mp_list_string += `${item.name}(${item.ipv4})`
+      if (index != mp_inventory.length -1 ) mp_list_string += `, ` 
+    })
+    
+    let ansible_inventory = []
+    mp_inventory.forEach((vm, index) => {
+      ansible_inventory.push(`ubuntu@${vm.ipv4}`)
+    })
+    
+    mutateDiscover(mp_list_string)
+    mutateStatus(ansible_inventory)
   })
-
-  let ansible_inventory = []
-  mp_inventory.forEach(vm => {
-    ansible_inventory.push(` ubuntu@${vm.ipv4}`)
-  })
-
-  if (ansible_inventory.length > 0) {
-    ansible_inventory += ','
-  }
-
-  mutateDiscover(mp_list_string)
-  mutateStatus(ansible_inventory)
 };
 
 
@@ -80,37 +98,29 @@ btnCreateVM.onclick = (e) => {
 };
 
 
-function runCommands(commands) {
-  if (!commands) { return 'no command sequence provided' }
-
-  let last_output = ''
-
-  console.log(`Running: ${commands[0]}`)
-  // Run command zero in the list
-  const cmd = exec(commands[0], (error, stdout, stderr) => {
-    if (error)  { last_output = error }
-    if (stderr) { last_output = stderr }
-    if (stdout) { last_output = stdout }
-  });
-
-  cmd.on('exit', (code) => {
-    if (code === 0) {
-      const newCommands = commands.slice(1)
-      newCommands.length === 0 ? last_output : runCommands(newCommands)
-    }
-  });
-} // runCommands(commands)
-
-
 btnSetupVM.onclick = (e) => {
-  // Commands in sequential order that they need to be run
-  let cmdSequence = [
-    `multipass copy-files ~/.ssh/id_rsa.pub tse3:/home/ubuntu/.ssh/passible_key`,
-    `multipass exec tse3 -- cp -n /home/ubuntu/.ssh/authorized_keys /home/ubuntu/.ssh/original_key`,
-    `multipass exec tse3 -- cp /home/ubuntu/.ssh/original_key /home/ubuntu/.ssh/authorized_keys`,
-    `multipass exec tse3 -- sed -i '$r /home/ubuntu/.ssh/passible_key' /home/ubuntu/.ssh/authorized_keys`,
-    `env ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -i 'ubuntu@10.203.58.62,' -e '{"ansible_python_interpreter":"/usr/bin/python3"}' ${__dirname + '/playbooks/hostnames.ansible'}`
-  ]
+  
+  const mp_list = runCommands([`multipass list`])
+  const mp_inventory = mpListAsObject(mp_list)  
+  
+  let cmdSequence = []  
+  
+  // Add Multipass commands for each machine that appears in the multipass list
+  mp_inventory.forEach(vm => {
+    cmdSequence.push(`multipass copy-files ~/.ssh/id_rsa.pub ${vm.name}:/home/ubuntu/.ssh/passible_key`)
+    cmdSequence.push(`multipass exec ${vm.name} -- cp -n /home/ubuntu/.ssh/authorized_keys /home/ubuntu/.ssh/original_key`)
+    cmdSequence.push(`multipass exec ${vm.name} -- cp /home/ubuntu/.ssh/original_key /home/ubuntu/.ssh/authorized_keys`)
+    cmdSequence.push(`multipass exec ${vm.name} -- sed -i '$r /home/ubuntu/.ssh/passible_key' /home/ubuntu/.ssh/authorized_keys`)
+  })
+
+  // Add an ansible inventory
+  let ansible_inventory = []
+  mp_inventory.forEach(vm => {
+    ansible_inventory.push(` ubuntu@${vm.ipv4}`)
+  })
+
+  // Setup hostnames across the inventory using ansible as the last command
+  cmdSequence.push(`env ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -i '${ansible_inventory},' -e '{"ansible_python_interpreter":"/usr/bin/python3"}' ${__dirname + '/playbooks/hostnames.ansible'}`)
 
   const result = runCommands(cmdSequence)
   console.log(result)
